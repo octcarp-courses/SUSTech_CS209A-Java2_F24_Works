aa7f731490a9c4853ae9379f0595cbc1ac202005
OctCarp <octcarp@hotmail.com>
2024-10-07 17:46:17 +0800

    Update Assignment 1 template

---
+++ Assignment1/src/OlympicsAnalyzer.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+public class OlympicsAnalyzer implements OlympicsAnalyzerInterface {
+
+    public OlympicsAnalyzer(String datasetPath) {
+
+    }
+
+    @Override
+    public Map<String, Integer> topPerformantFemale() {
+        return Map.of();
+    }
+
+    @Override
+    public Map<String, Float> bmiBySports() {
+        return Map.of();
+    }
+
+    @Override
+    public Map<String, Set<Integer>> leastAppearedSport() {
+        return Map.of();
+    }
+
+    @Override
+    public Map<String, Integer> winterMedalsByCountry() {
+        return Map.of();
+    }
+
+    @Override
+    public Map<String, Integer> topCountryWithYoungAthletes() {
+        return Map.of();
+    }
+}
