c7c4912cf3bf00141c70de8f76988c1be612c9ad
OctCarp <octcarp@hotmail.com>
2024-12-02 22:53:22 +0800

    clean git sub module

--- Assignment2/game-common/src/main/java/module-info.java
+++
@@ -1,4 +0,0 @@
-module linkgame.common {
-    exports io.github.octcarp.sustech.cs209a.linkgame.common.model;
-    exports io.github.octcarp.sustech.cs209a.linkgame.common.packet;
-}
--- Assignment2/game-server/src/main/java/module-info.java
+++
@@ -1,6 +0,0 @@
-module linkgame.server {
-    requires transitive linkgame.common;
-    requires org.apache.commons.csv;
-    exports io.github.octcarp.sustech.cs209a.linkgame.server;
-    exports io.github.octcarp.sustech.cs209a.linkgame.server.net;
-}
